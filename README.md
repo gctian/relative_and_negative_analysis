d2c3e7d23827fc80d6a48af8c971647efe76348e
tgc <tgc_forward@163.com>
2017-02-24 19:26:25 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+﻿# 项目说明
+
+
+
+
+### 1. 环境部署
+
+ - Ubuntu/CentOS环境
+ - pip install jieba
+ - pip install pandas
+ - pip install numpy
+ - pip insatll scipy
+ - pip install -U scikit-learn
+
+### 2. 相关性分析
+
+    
+执行根目录下negative_analysis.py文件，在根目录下python negative_analysis.py即可，然后等待加载模型，模型加载成功后给出输入一段文本的提示，
+输入一段和国网有关的文本，模型会给出预测结果。0表示国网负面，1表示国网正面。输入quit退出程序。
+
+### 3. 负面舆情识别
+
+执行根目录下negative_analysis.py文件，在根目录下python negative_analysis.py即可，然后等待加载模型，模型加载成功后给出输入一段文本的提示，
+输入一段和国网有关的文本，模型会给出预测结果。0表示国网负面，1表示国网正面。输入quit退出程序。
+
+### 4. 预处理及训练
+此版本是加载训练好的模型，直接对一句话进行预测。关于具体的预处理及训练过程，请见StateGrid项目下的text_classify文件夹。
---
+++ negative_analysis.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+import sys
+import numpy as np
+import cPickle as pickle
+import jieba
+import datetime
+
+def load_model(model_path):
+	with open(model_path,'rb') as f:
+		clfs = pickle.load(f)
+	return clfs
+
+def read_stopdict(stopword_file):
+	stopword_dict = set([])
+	f = open(stopword_file, 'rb')
+	for line in f:
+		line = line.strip('\r\n').decode('utf-8')
+		stopword_dict.add(line)
+	f.close()
+	return stopword_dict
+
+
+def load_vec_fit(path):
+    with open(path, 'rb') as f:
+        d = pickle.load(f)
+    return d
+
+def vectorize_test(test_x,data_path):
+    vect_fit = load_vec_fit(data_path)
+    test_vec = vect_fit.transform(test_x)
+
+    return test_vec
+
+def negative_predict(test_x, clfs):
+
+    result = None
+    label = 0
+    for key in clfs:
+        model = clfs[key]
+        if model == None:
+            continue
+        pre = model.predict(test_x)
+        if result is None:
+            result =pre
+        else:
+            result += pre
+    if result >2 :
+        label = 1
+    return label
+
+def seg_text(str):
+    stopword_file = './data/all_stopword.txt'
+    stopword_dict = read_stopdict(stopword_file)
+    str_seg = jieba.cut(str.strip('\n'))
+    str_words = []
+    for word in str_seg:
+        if word == ' ' or word in stopword_dict:
+            continue
+        str_words.append(word)
+
+    seg_words = ' '.join(str_words)
+    str_words = []
+    str_words.append(seg_words)
+    test_x = np.asarray(str_words)
+
+    return test_x
+
+
+if __name__ == '__main__':
+
+    sys.stderr.write('load model...\n')
+
+
+    data_path = './data/negative_train_1201'
+
+    model_path = './data/negative_model_1201'
+
+    clfs = load_model(model_path)
+    if clfs == None:
+        sys.std.write('Error:load model error!')
+        sys.exit(1)
+    sys.stderr.write('Info: load model successfully!\n')
+
+    while True:
+
+        str = raw_input('please input some str:\n')
+        if str == 'quit':
+            sys.exit(1)
+        start = datetime.datetime.now()
+        sentence_x = seg_text(str)
+        test_vec = vectorize_test(sentence_x, data_path)
+        label = negative_predict(test_vec,clfs)
+        end = datetime.datetime.now()
+        print 'predict label:%d, time:%ss '%(label, (end-start).seconds)
+
